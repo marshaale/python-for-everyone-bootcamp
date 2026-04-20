9f1990d7c2be00c10449c18bc36a6b5bc2209d74
mdev <modev.404@gmail.com>
2026-04-20 20:25:42 +0300

    examples of 02 sections 1 & 2

---
+++ myexamples/02_control_flow/main.py
@@ -0,0 +1,40 @@
+print("Hello,"+"world")
+print("Hi salma "*3)
+
+
+area = 10
+width = 5
+
+print("Area + Width",area+width)
+print("Area - Width",area-width)
+print("Area / Width",area/width) # Division result of float.
+print("Area // Width",area // width) # Division result of integer.
+print("Area * Width",area*width)
+print("Area ^ Width",area ** width)
+
+people = 9
+shared_amount = 100
+remainder = shared_amount % people
+
+print("Remainder of",shared_amount,"out of",people,"People is",remainder)
+
+
+counter = 0
+counter = counter + 10
+print("First addition:",counter)
+
+counter -= 10
+
+print("Subtracted by 10 using assignment operator:",counter)
+
+counter += 7
+
+print("Second addition using assignment operator:",counter)
+
+counter *= 3
+
+print("Multiplied by 3 using assignment operator:",counter)
+
+counter //= 3
+
+print("Divission",counter)
---
+++ myexamples/myminipractices/02_control_flow/main.py
@@ -0,0 +1,18 @@
+print(11+5)
+print(3/2)
+print(3//2)
+print("Hello"*3)
+
+balance = 100
+
+balance -= 25
+
+balance += 10
+
+print("Your balance is",balance)
+
+word = "Hi"
+
+word += " there here we are."
+
+print(word)
