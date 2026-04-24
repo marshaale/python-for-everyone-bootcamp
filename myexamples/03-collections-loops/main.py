5a8f5be75e1a3daff97b54ce0dba6a63b213ae98
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-04-24 18:01:55 +0300

    completed tuple & sets practice

--- myexamples/03-collections-loops/main.py
+++ myexamples/03-collections-loops/main.py
@@ -30,3 +30,45 @@ print(scores[1:])
 print(scores[:4])
 
 print(scores[0:4:2])
+
+points = (75,88)
+
+print(points[0],points[1])
+
+x,y = points
+
+print("X=",x,"Y=",y)
+
+print(type(points))
+
+single_tuple = (66,)
+print(type(single_tuple))
+
+tags = {"python","code","learn","python","js","ts"}
+print(tags)
+
+tags.add("fun")
+# tags.remove('java')
+tags.discard('java')
+
+print(tags)
+
+print("fun" in tags)
+
+python = {"oop","interpreter","class","type dynamic"}
+java = {"oop","compiler","class","type safe"}
+
+print(python)
+print(java)
+
+print("Python union java",python | java)
+print("Python intersect java",python & java)
+print("Python difference java",python - java)
+print("Java difference Python",java - python)
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+print("A union B",a | b)
+print(a-b)
+print(a&b)
---
+++ myexamples/myminipractice/03-collections-loops/sets_practice.py
@@ -0,0 +1,11 @@
+letters = ["a","b","a","c","b"]
+
+set_letters = set(letters)
+
+print(set_letters)
+
+last_year_stats = {65,76,45,91}
+this_year_stats = {76,88,95,65}
+
+print(last_year_stats & this_year_stats)
+print(last_year_stats - this_year_stats)
