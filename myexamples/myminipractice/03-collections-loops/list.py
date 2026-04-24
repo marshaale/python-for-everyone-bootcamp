87a0001d8d0ee9048904d47b151d990ab729e21f
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-04-24 17:26:22 +0300

    complete list examples and mini practice

---
+++ myexamples/myminipractice/03-collections-loops/list.py
@@ -0,0 +1,17 @@
+names = ["Alan","Ada","Lin"]
+print(names)
+names[1] = "Aisha"
+print(names)
+names.remove("Alan")
+print(names)
+
+top_scores = [100,90,95]
+mid_scores = [75,65,60]
+
+scores = top_scores + mid_scores
+
+print(scores)
+
+words = ["one", "two", "three", "four"]
+
+print(words[1:3])
