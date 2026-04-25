9a1bd8a0f35ddeff73910ec6ab781c414fe60ed7
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-04-25 17:42:13 +0300

    completed coordinates.py

---
+++ myexamples/03-collections-loops/home-work/coordinates.py
@@ -0,0 +1,10 @@
+# tuple point
+point = (45,90)
+# Unpack the tuple x and y
+x,y = point
+
+print("X=",x,"Y=",y)
+
+single_value_tuple = (68,)
+
+print("type of single_value_tuple:",type(single_value_tuple))
