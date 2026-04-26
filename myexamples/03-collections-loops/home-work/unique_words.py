4792a9713789146ed1416b07f72d764192164e82
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-04-26 06:31:56 +0300

    completed unique words / set

---
+++ myexamples/03-collections-loops/home-work/unique_words.py
@@ -0,0 +1,8 @@
+# List of words
+words = ["python","learn","love","code","love","python"]
+print("original words",words)
+# set of unique words
+unique_words = set(words)
+print("unique words",unique_words)
+
+print("is python is in unique words:","python" in unique_words)
