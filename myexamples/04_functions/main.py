cd613cf75f1c443dd62dd127ef90c19aaa4f1ee6
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-04-28 06:55:08 +0300

    completed 04/01 practice & examples

---
+++ myexamples/04_functions/main.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print("Hi",name)
+
+greet("Hassan")
+
+
+def say_good_night(name):
+    print(f"Good night {name}....")
+
+say_good_night("Jordan")
+
+def add(a,b):
+    print(f"{a} + {b} = {a+b}")
+
+add(12,10)
---
+++ myexamples/myminipractice/04_functions/main.py
@@ -0,0 +1,14 @@
+def square(n):
+    return n*n
+
+print(f"Square of 4 is {square(4)}")
+print(f"Square of 7 is {square(7)}")
+
+def shout(text):
+    print(text.upper())
+
+shout("marshaale")
+shout("tood")
+
+# Function defining is when you create the function. it exists but does not executes result.
+# Function calling is when you use the function.
