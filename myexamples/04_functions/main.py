b74fa6b8eb4dd96c89bdf4b955fba195bcd94677
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-04-29 20:31:45 +0300

    practice and example

--- myexamples/04_functions/main.py
+++ myexamples/04_functions/main.py
@@ -27,3 +27,32 @@ def item_details(name,price,in_stock):
     print(f"{name}, {in_stock} is remaining in stock and per price is ${price}")
 
 item_details(in_stock=5,name="iphone xr",price=189)
+
+def calculate_price(price):
+    discount = 0.1
+    if price >= 100:
+        return price - (price * discount)
+    return price
+
+print(calculate_price(100))
+print(calculate_price(90))
+
+
+def repeat(word,times=2):
+    print(word*times)
+
+repeat("Hi ")
+repeat("hh",10)
+
+print(*[1,2,3])
+
+def summarize(*args):
+    return sum(args)
+
+print(summarize(1,2,3))
+print(summarize(1,2,3,5,7))
+
+def payment_details(**info):
+    return info
+
+print(payment_details(method="Evc",sender_phone="1234788",receiver_phone="12349104",amount=111,datetime="05-05-2025 14:30"))
--- myexamples/myminipractice/04_functions/main.py
+++ myexamples/myminipractice/04_functions/main.py
@@ -17,3 +17,48 @@ def introduce(first,last):
     print(first,last)
 
 introduce(last="Ahmed",first="Muktar")
+
+def is_even(number):
+    return number % 2 == 0
+
+print(is_even(100))
+print(is_even(7))
+
+def max_of_two(a,b):
+    if a > b:
+        return a
+    if b > a:
+        return b
+    return
+
+print(max_of_two(10,10))
+print(max_of_two(10,11))
+print(max_of_two(100,10))
+
+def stats(a,b):
+    return a+b,b*a
+
+a,b = stats(10,22)
+
+print(a)
+print(b)
+
+def divider(a,b=2):
+    return a / b
+
+print(divider(10))
+
+def data(x,items=None):
+    if not items:
+        items = [1,2,3,4]
+    return x,items
+
+print(data(22))
+print(data(33,[1,7]))
+
+def first_and_rest(head,*tail):
+    return f"Head {head}",f"Rest count {len(tail)}"
+
+print(first_and_rest(3,1,2,4,5,6,7,8))
+print(first_and_rest(5,2,4,7,3,8))
+
